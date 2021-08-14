67c1308214497dae231eb54ec3823c74aa553ac6
juanraugutierrez <juan.gutierrezcalquin@gmail.com>
2021-08-14 11:56:41 -0400

    7
    trabajo-sabado

--- sources/rutinas.js
+++ sources/rutinas.js
@@ -1,4 +1,19 @@
-window.onload = function() {
-    alert("Hola Bienvenidos a esta pagina");
+// window.onload = function() {
+//     alert("Hola Bienvenidos a esta pagina");
 
+// }
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+	}
+  
+
+    document.getElementById("dato").innerHTML ="Color de fondo: <b>"+ color+"</b>"; 
+	document.body.style.background = color;
+   
 }
