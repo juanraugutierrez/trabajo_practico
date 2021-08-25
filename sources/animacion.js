94828f97876e6406c74379b2029f1d6ca32b5fff
juanraugutierrez <juan.gutierrezcalquin@gmail.com>
2021-08-24 23:29:05 -0400

    3
    otro

--- sources/animacion.js
+++ sources/animacion.js
@@ -1,19 +1,13 @@
-var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-var interval = setInterval(function(){ 
-    $('img-animated').addClass('animated bounce').one(animationEnd, function() {
-      $(this).removeClass ('animated bounce');
-    });
-}, 1000);
 
-$('start-button').click(function() {
-  clearInterval(interval);
-    $(this).addClass('animated bounceOutUp').one(animationEnd, function() {
-        $('.title').show();
-        $('.title').addClass('animated fadeIn').one(animationEnd, function() {
-          $('.img-animated').addClass('animated flip').one(animationEnd, function() {
-            $('.content').addClass('animated rubberBand');            
-          });
-      });
-    });
-});
+// Use the begin callback.
+$("#anima").velocity(
+  { 
+    scale: 1.5
+  },
+  { 
+    duration: 2000,
+    begin: function() { 
+       alert("This is called before the animation begins.");
+    }
+  });
